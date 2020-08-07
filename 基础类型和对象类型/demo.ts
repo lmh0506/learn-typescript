a708d45b82052d3b8ad72111a9cee616600b3ff3
lmh <495453865@qq.com>
2020-08-07 15:16:36 +0800

    init

---
+++ 基础类型和对象类型/demo.ts
@@ -0,0 +1,20 @@
+// 基础类型 null undefined symbol boolean void
+const count: number = 123
+const myName: string = 'test'
+
+// 对象类型
+const man: {
+  name: string,
+  age: number
+} = {
+  name: 'aaa',
+  age: 8
+}
+
+const arr: number [] = [123, 213]
+
+class Person {}
+
+const dell: Person = new Person
+
+const getTotal: () => number = () => 123
